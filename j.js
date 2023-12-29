68488cb24c779d381ccf82ee1e837482ae39b537
Zane Schaffer <znschaffer@gmail.com>
2023-12-29 10:40:49 -0800

    add titles

--- j.js
+++ j.js
@@ -1,71 +1,82 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioContext = new AudioContext()
+const audioContext = new AudioContext();
 const db = new Dexie("audioDB");
 const playButton = document.querySelector(".tape-controls-play");
+const informationPanel = document.getElementById("informationPanel");
 
 const audio = new Audio();
 db.version(3).stores({
-      sounds: `
+  sounds: `
         id,
         data,
         audio,
         url`,
-    });
-const audioFiles = document.getElementsByClassName("item")
+});
+const audioFiles = document.getElementsByClassName("item");
 const visualizeAudio = (url, id) => {
-  db.sounds.get({id:id})
-    .then(sound => sound.data)
-    .then(data => draw(data,id))
-    .catch(_ => fetch(url)
-                  .then(response => response.arrayBuffer())
-                  .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
-                  .then(audioBuffer => normalizeData(filterData(audioBuffer)))
-                  .then(data => db.sounds.add({id: id, data: data, url: url}) && draw(data,id)))
-}
+  db.sounds
+    .get({ id: id })
+    .then((sound) => sound.data)
+    .then((data) => draw(data, id))
+    .catch((_) =>
+      fetch(url)
+        .then((response) => response.arrayBuffer())
+        .then((arrayBuffer) => audioContext.decodeAudioData(arrayBuffer))
+        .then((audioBuffer) => normalizeData(filterData(audioBuffer)))
+        .then(
+          (data) =>
+            db.sounds.add({ id: id, data: data, url: url }) && draw(data, id),
+        ),
+    );
+};
 
-const filterData = audioBuffer => {
+const filterData = (audioBuffer) => {
   const rawData = audioBuffer.getChannelData(0);
   const samples = 50;
   const blockSize = Math.floor(rawData.length / samples);
-  const filteredData = []
+  const filteredData = [];
   for (let i = 0; i < samples; i++) {
     let blockStart = blockSize * i;
     let sum = 0;
     for (let j = 0; j < blockSize; j++) {
-      sum = sum + Math.abs(rawData[blockStart + j])
+      sum = sum + Math.abs(rawData[blockStart + j]);
     }
-    filteredData.push(sum/blockSize);
+    filteredData.push(sum / blockSize);
   }
 
   return filteredData;
-}
+};
 
-const normalizeData = filteredData => {
+const normalizeData = (filteredData) => {
   const multiplier = Math.pow(Math.max(...filteredData), -1);
-  return filteredData.map(n => n * multiplier);
-}
+  return filteredData.map((n) => n * multiplier);
+};
 
 const draw = (normalizedData, id) => {
   // Set up the canvas
   const canvas = document.getElementById(id);
-  console.log(canvas)
   canvas.addEventListener("click", (ev) => {
-
     const old = document.querySelector('[aria-selected="true"]');
     if (old && old == ev.target) {
-      audio.pause()
-      ev.target.ariaSelected = false
-      return
+      audio.pause();
+      informationPanel.classList.toggle("hidden");
+      ev.target.ariaSelected = false;
+      return;
     } else if (old) {
-    old.ariaSelected = false
+      old.ariaSelected = false;
     }
 
+    if (informationPanel.classList.contains("hidden")) {
+      informationPanel.classList.toggle("hidden");
+    }
+    informationPanel.firstChild.textContent = ev.target.id;
 
-    audio.pause()
-    audio.src = ev.target.dataset.url
-    audio.play()
-    ev.target.ariaSelected = true
-  })
+    audio.pause();
+    audio.src = ev.target.dataset.url;
+    audio.play();
+    ev.target.ariaSelected = true;
+  });
+
   const dpr = window.devicePixelRatio || 1;
   const padding = 50;
   canvas.width = canvas.offsetWidth * dpr;
@@ -80,9 +91,9 @@ const draw = (normalizedData, id) => {
     const x = width * i;
     let height = normalizedData[i] * canvas.offsetHeight - padding;
     if (height < 0) {
-        height = 0;
+      height = 0;
     } else if (height > canvas.offsetHeight / 2) {
-        height = height > canvas.offsetHeight / 2;
+      height = height > canvas.offsetHeight / 2;
     }
     drawLineSegment(ctx, x, height, width, (i + 1) % 2);
   }
@@ -95,12 +106,11 @@ const drawLineSegment = (ctx, x, y, width, isEven) => {
   y = isEven ? y : -y;
   ctx.moveTo(x, 0);
   ctx.lineTo(x, y);
-  ctx.arc(x + width , y, width , Math.PI, 0, isEven);
+  ctx.arc(x + width, y, width, Math.PI, 0, isEven);
   ctx.lineTo(x + width, 0);
   ctx.stroke();
 };
 
-for(const audioFile of audioFiles) {
-  visualizeAudio(audioFile.dataset.url, audioFile.id)
+for (const audioFile of audioFiles) {
+  visualizeAudio(audioFile.dataset.url, audioFile.id);
 }
-
